3841db323ed2f3100d458d98ff31f99492ddb118
이응수 <eungsu@me.com>
2024-12-17 16:05:07 +0900

    Update HelloController.java

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -2,13 +2,18 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Value;
 
 @Controller
 public class HelloController {
 
+	@Value("${message.user.name}")
+	private String name;
+
 	@RequestMapping("/")
-	public String home() {
-		
+	public String home(Model model) {
+		model.addAttribute("name", name);
 		return "home";
 	}
 }
